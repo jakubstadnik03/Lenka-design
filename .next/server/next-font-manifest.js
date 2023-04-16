f49503ebb6013cb3b36841af8f3022bceb7cdb06
Jakub Stadnik <106155603+jakubstadnik03@users.noreply.github.com>
2023-04-16 23:08:39 +0200

    d

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,5 +1,7 @@
 self.__NEXT_FONT_MANIFEST={
-  "pages": {},
+  "pages": {
+    "/exkluzivni-nabidky": []
+  },
   "app": {},
   "appUsingSizeAdjust": false,
   "pagesUsingSizeAdjust": false
--- pages/exkluzivni-nabidky.js
+++ pages/exkluzivni-nabidky.js
@@ -1,7 +1,132 @@
-import React from "react";
+import LightGallery from "lightgallery/react";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
 import { Carousel, Image, Button, Container } from "react-bootstrap";
 import Link from "next/link";
+
 const exkluzivniNabidky = () => {
+  const images = [
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera2.jpeg",
+      thumbnail: "img/prianera/prianera2-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera3.jpeg",
+      thumbnail: "img/prianera/prianera3-small.jpeg",
+    },
+
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera4.jpeg",
+      thumbnail: "img/prianera/prianera4-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera5.jpeg",
+      thumbnail: "img/prianera/prianera5-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera6.jpeg",
+      thumbnail: "img/prianera/prianera6-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera7.jpeg",
+      thumbnail: "img/prianera/prianera7-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera8.jpeg",
+      thumbnail: "img/prianera/prianera8-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera9.jpeg",
+      thumbnail: "img/prianera/prianera9-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera10.jpeg",
+      thumbnail: "img/prianera/prianera10-small.jpeg",
+    },
+
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera12.jpeg",
+      thumbnail: "img/prianera/prianera12-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera13.jpeg",
+      thumbnail: "img/prianera/prianera13-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera14.jpeg",
+      thumbnail: "img/prianera/prianera14-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera15.jpeg",
+      thumbnail: "img/prianera/prianera15-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera16.jpeg",
+      thumbnail: "img/prianera/prianera16-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera17.jpeg",
+      thumbnail: "img/prianera/prianera17-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera18.jpeg",
+      thumbnail: "img/prianera/prianera18-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera19.jpeg",
+      thumbnail: "img/prianera/prianera19-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera20.jpeg",
+      thumbnail: "img/prianera/prianera20-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera21.jpeg",
+      thumbnail: "img/prianera/prianera21-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera22.jpeg",
+      thumbnail: "img/prianera/prianera22-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera23.jpeg",
+      thumbnail: "img/prianera/prianera23-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera24.jpeg",
+      thumbnail: "img/prianera/prianera24-small.jpeg",
+    },
+    {
+      alt: "Nábytek Prianera",
+      image: "img/prianera/prianera25.jpeg",
+      thumbnail: "img/prianera/prianera25-small.jpeg",
+    },
+  ];
   return (
     <main>
       <div style={{ position: "relative" }}>
@@ -68,18 +193,16 @@ const exkluzivniNabidky = () => {
         <div>
           <h2>Kvalitní italský nábytek možno vyzkoušet!</h2>
           <p>
-            Řemeslný duch ve své nejčistší a nejvyšší podobě, který oživuje
-            výrobu společnosti. Skutečný kód dokonalosti spojený s
-            nezaměnitelným italským know-how, kterým se vyznačuje tvorba výrobků
-            kolekce Prianera.
+            Výrobky firmy Prianera jsou ručně vyrobené v dokonalé kvalitě, dle
+            návrhů italských designérů. Mají nezaměnitelné italské know-how.
+            Jsou velmi pohodlné a přinesou Vám do Vašeho domova luxus a pohodlí.
           </p>
           <p>
             Italský nábytek je možné si vyzkoušet a prohlédnout v mém vzorovém
             bytě v Liberci
           </p>
           <p>
-            Podrobný katalog s veškerými podklady a fotografiemi je možno
-            stáhnout a na vyzkoušení a ošahání materiálů mě můžete kontaktovat!
+            Podrobný katalog s veškerými podklady a fotografiemi naleznete zde.
           </p>
           <Link href="img/prianera/katalog.pdf" target="_blank">
             <button type="button" className="btn btn-outline-primary">
@@ -101,6 +224,20 @@ const exkluzivniNabidky = () => {
           />
         </Link>
       </div>
+      <section className="max-width">
+        <h2>Fotogalerie produktů od firmy Prianera</h2>
+        <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
+          {images.map((item, index) => (
+            <a href={item.image} key={index}>
+              <img
+                src={item.thumbnail}
+                className="gallery-img"
+                loading="lazy"
+              />
+            </a>
+          ))}
+        </LightGallery>
+      </section>
     </main>
   );
 };
